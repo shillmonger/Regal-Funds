414c8c0864c0bafbf39c2130437dfb042b35400a
Bby Lion <codelab042@gmail.com>
2025-10-07 15:52:38 +0100

    first commit

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,16 @@
-export { default } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
 
-export const config = {
-  matcher: ["/user-dashboard/:path*"],
-};
-
-
+export async function middleware(req: NextRequest) {
+const currentPath = req.nextUrl.pathname;
+const authUrl = '/auth/login';
+if (currentPath.startsWith('/user-dashboard')) {
+  const token =  await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  if (!token) {
+    return NextResponse.redirect(new URL(authUrl, req.url));
+  }
+  //you can do role based authentication here
+  return NextResponse.next();
+}
+return NextResponse.next();
+}
